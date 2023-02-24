2312890f6006cefed4d8838e165f6691f2036df2
Alistair Abao <abaolester@gmail.com>
2023-02-24 01:59:27 +0100

    added gallery and some layout changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import classes from './Navbar.module.css'
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
 
-const Navbar = () => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const MyNavbar = () => {
     return (
-        <div className={classes.navbar}>
-            <div className={classes.navbarLinks}>
-                <Link to="/">HOME</Link>
-                <a href="/gallery">GALLERY</a>
-                <Link to="/rates">RATES</Link>
-                <a href="/contact">CONTACT</a>
-            </div>
-        </div >
+        <div>
+            <Navbar bg="dark" variant="dark" className='fixed-top'>
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <LinkContainer to='/'>
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to='/gallery'>
+                            <Nav.Link>Gallery</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to='/rates'>
+                            <Nav.Link>Rates</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+        // <nav className={classes.navbar}>
+        //     <div className={classes.navbarLinks}>
+        //         <Link to="/">HOME</Link>
+        //         <Link to="/gallery">GALLERY</Link>
+        //         <Link to="/rates">RATES</Link>
+        //         <a href="/contact">CONTACT</a>
+        //     </div>
+        // </nav >
     );
 }
 
-export default Navbar;
+export default MyNavbar;
